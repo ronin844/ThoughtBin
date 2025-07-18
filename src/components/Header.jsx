ccdd5744144475d4865edb9a290a15b3973786b7
Prasoon singh <144701200+ronin844@users.noreply.github.com>
2025-07-18 20:16:48 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import HighlightIcon from '@mui/icons-material/Highlight';
 
 function Header(){
     return <header>
-        <h1><HighlightIcon />NoteStack</h1>
+        <h1><HighlightIcon />ThoughtBin</h1>
     </header>
 }
 export default Header;
